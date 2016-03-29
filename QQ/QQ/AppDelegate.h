074bd3eebc6d359bb17d661469e21360f1669cba
chenshengwen <tarena01@tarena01.local>
2016-03-29 17:30:24 +0800

    “初始化项目”.

---
+++ QQ/QQ/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  QQ
+//
+//  Created by tarena01 on 16/3/29.
+//  Copyright © 2016年 tarena01. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
